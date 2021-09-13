8e59ad6dfa360cf2e35dfd74559a229591b2a0f2
normyp <pm.normington@gmail.com>
2021-09-13 14:44:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bluetoothapp/DisplayMessageActivity.java
@@ -0,0 +1,29 @@
+package com.example.bluetoothapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    public void printMessage(View view){
+        Log.d("DisplayMessageActivity", "Happy Birthday");
+    }
+}
